0f50925de2640eb07bcd7a8003ca1c83b16ea913
mambar7 <malibongwemambar@gmail.com>
2020-07-17 11:56:33 +0200

    APDV400 Test Q3 commit

---
+++ Desktop/Test_Q3/src/test_q3/Mamba201505019.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_q3;
+
+/**
+ *
+ * @author mambar
+ */
+public class Mamba201505019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 8;
+        int num2 = 4;
+        int sum = num1 + num2;
+        System.out.println("The sum of "+num1+" and "+num2+" is equal to "+sum);
+    }
+    
+}
